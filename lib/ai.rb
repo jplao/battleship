2a2fd5e02c2adca34830388f47cdd6e831bc251c
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-02 10:35:24 -0600

    add randomized function for ai guesses

--- lib/ai.rb
+++ lib/ai.rb
@@ -7,7 +7,7 @@ class AI
     @guesses = []
   end
 
-  def guess(coordinates)
+  def guess(coordinates = ('A'..'D').to_a.sample + rand(1..4).to_s)
     @guesses << coordinates.to_s
     coordinates.to_s
   end
