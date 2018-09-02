f88d2036d91591d05033d16746a6cd47beb5505a
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-02 09:18:04 -0600

    Added gameplay class, moved intro, and added more responses

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,17 +1,9 @@
-require './lib/board'
+require './lib/gameplay'
 
 puts "Welcome to BATTLESHIP"
 puts
 puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
 response = gets.chomp
 
-if ["p", "play"].include? response.to_s
-  puts "Lets's start a new game!"
-  board = Board.new
-  board.new_game_board
-elsif ["i", "instructions"].include? response.to_s
-  instructions = File.open("./lib/instructions.txt", "r")
-  puts instructions.read
-elsif ["q", "quit"].include? response.to_s
-  puts "See you next time!"
-end
+gameplay = GamePlay.new
+gameplay.intro(response)
