dd6ed0f9d3bb35e6db48b09d61393a982640be15
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-04 11:40:33 -0600

    Test it can validate if destroyer is placed off map

--- lib/validate.rb
+++ lib/validate.rb
@@ -1,13 +1,15 @@
 require './lib/board'
 
 class Validate
-  def validate_destoyer_placement(input)
+
+  def validate_destroyer_placement(input)
+    board = Board.new
     ship_coords = input.split
-    start_coords = board.location_keys(ship_coords[0])
-    end_coords = board.location_keys(ship_coords[1])
+    start_coords = board.location_keys[ship_coords[0]]
+    end_coords = board.location_keys[ship_coords[1]]
     if start_coords == nil || end_coords == nil
       "That location does not exist."
-    end 
+    end
   end
 
 end
--- test/validate_test.rb
+++ test/validate_test.rb
@@ -11,4 +11,15 @@ class ValidateTest < Minitest::Test
     validate = Validate.new
     assert_instance_of Validate, validate
   end
+
+  def test_it_can_validate_destroyer_placement
+    validate = Validate.new
+
+    actual = validate.validate_destroyer_placement("D6 B4")
+    assert_equal "That location does not exist.", actual
+
+    actual = validate.validate_destroyer_placement("A3 F1")
+    assert_equal "That location does not exist.", actual
+
+  end
 end
