0a1e72d7074d84f0970db3bd4f3b812f09f66438
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-03 20:47:38 -0600

    transfer intro to battleship class and add initial methods for most of game sequence

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,9 +1,29 @@
 require './lib/gameplay'
 
-puts "Welcome to BATTLESHIP"
-puts
-puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-response = gets.chomp
+class Battleship
 
-gameplay = GamePlay.new
-gameplay.intro(response)
+  def intro(response = gets.chomp)
+    if ["p", "play"].include? response.to_s
+      puts "Lets's start a new game!"
+      new_game = GamePlay.new
+      new_game.ship_layout
+    elsif ["i", "instructions"].include? response.to_s
+      instructions = File.open("./lib/instructions.txt", "r")
+      print instructions.read
+      print "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+      intro
+    elsif ["q", "quit"].include? response.to_s
+      puts "See you next time!"
+    else
+      print "That is not a valid response, please try again"
+      print "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+      intro
+    end
+  end
+
+end
+
+game = Battleship.new
+puts "Welcome to BATTLESHIP"
+puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+game.intro
