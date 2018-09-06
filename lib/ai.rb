5616a48e98d77f55bcf825a834bce9ba9c095251
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-05 19:24:13 -0600

    worked on ai guesses

--- lib/ai.rb
+++ lib/ai.rb
@@ -9,14 +9,14 @@ class AI
     @ships = []
   end
 
-#  def guess(coordinates = ('A'..'D').to_a.sample + rand(1..4).to_s)
-#    if @guesses.include?(coordinates.to_s)
-#      #guess
-#    else
-#      @guesses << coordinates.to_s
-#    end
-#    coordinates.to_s
-#  end
+  def guess(coordinates = ('A'..'D').to_a.sample + rand(1..4).to_s)
+    if @guesses.include?(coordinates.to_s)
+      guess
+    else
+      @guesses << coordinates.to_s
+    end
+    coordinates
+  end
 
   def place_destroyer
     start_coords = randomize_start_location_of_ship
@@ -58,7 +58,6 @@ class AI
       "vertical"
     end
   end
-
 
   def add_ships
     destroyer = Ship.new(2)
